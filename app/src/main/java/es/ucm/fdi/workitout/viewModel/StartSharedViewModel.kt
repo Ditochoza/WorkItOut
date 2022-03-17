4657476230eb07067f2c8b11f7f81250f0a07dd1
Zaka-gh <79411156+Zaka-gh@users.noreply.github.com>
2022-03-17 23:19:46 +0100

    prueba firebase sign up

--- app/src/main/java/es/ucm/fdi/workitout/viewModel/StartSharedViewModel.kt
+++ app/src/main/java/es/ucm/fdi/workitout/viewModel/StartSharedViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.SavedStateHandle
+import com.google.firebase.firestore.FirebaseFirestore
 import es.ucm.fdi.workitout.model.User
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -14,6 +15,8 @@ class StartSharedViewModel(application: Application, private val savedStateHandl
     private val _user = MutableStateFlow(savedStateHandle.get(::user.name) ?: User())
     val user: StateFlow<User> = _user.asStateFlow()
 
+    private val db = FirebaseFirestore.getInstance()
+
     fun getLoginCredentials(email: String, password: String) {
         Log.d("ViewModel", "1$email $password")
         Log.d("ViewModel", "2${user.value.email} ${user.value.tempPassword}")
@@ -21,6 +24,31 @@ class StartSharedViewModel(application: Application, private val savedStateHandl
 
     fun register(name: String, email: String, password: String, passwordValidate: String) {
 
+        var error = false;
+        //Comprobar que los valores introducidos por el usuario no son vacios
+        if (name.isEmpty()){
+            error = true;
+        }
+
+        if (email.isEmpty()){
+            error = true;
+        }
+
+        if (password.isEmpty()){
+            error = true;
+        }
+
+        if (passwordValidate.isEmpty()){
+            error = true;
+        }
+
+        //Comprobar contraseña y guardar en la base de datos
+        if (!error  && password == passwordValidate){
+            db.collection("users").document(email).set(
+                hashMapOf("Name" to name,
+                    "password" to password)
+            )
+        }
     }
 
     fun saveStateHandle() {
