00d735c679399a4c5f1dc432a5db686e44a9707a
Zaka-gh <79411156+Zaka-gh@users.noreply.github.com>
2022-03-20 00:43:20 +0100

    Create Exercises 1/?

--- app/src/main/java/es/ucm/fdi/workitout/view/MainActivity.kt
+++ app/src/main/java/es/ucm/fdi/workitout/view/MainActivity.kt
@@ -8,9 +8,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val intent = Intent(this, StartActivity::class.java)
+        val intent = Intent(this, EditActivity::class.java)
         startActivity(intent)
-        finish()
+        //finish()
+
+        /*val intent = Intent(this, StartActivity::class.java)
+        startActivity(intent)
+        finish()*/
 
         //setContentView(R.layout.activity_main)
     }
